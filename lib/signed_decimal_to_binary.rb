0af42ec3106ff2e596e9d0355dcb6d237f149bfa
Esteba Arango <marranoparael31@gmail.com>
2015-04-04 01:34:17 -0500

    Implement .to_negative

--- lib/signed_decimal_to_binary.rb
+++ lib/signed_decimal_to_binary.rb
@@ -1,11 +1,41 @@
 class SignedDecimalToBinary
   def self.convert decimal
     return '0' if decimal == 0
+
+    # Flag if the number is negative
+    negative = decimal < 0
+
+    # Set the number to positive
+    decimal = decimal.abs
+
     binary = ''
     while decimal != 0
       binary += (decimal % 2).to_s
       decimal /= 2
     end
-    binary.reverse
+    binary.reverse!
+    negative ? to_negative(binary) : binary
   end
+
+  def self.to_negative(binary)
+    # Add extra byte
+    binary = '0' + binary
+    # Perform the NOT operand
+    not_binary = binary.chars.map{|c| (c.to_i - 1).abs }.reverse
+
+    # Sum 1
+    summed_binary = not_binary
+    summed_binary.each_with_index do |bit_num, index|
+      summed_binary[index] = (bit_num == 1) ? 0 : 1
+      break if summed_binary[index] == 1
+    end
+
+    summed_binary.reverse!
+
+    # Remove extra byte
+    summed_binary = summed_binary[1..-1] if (summed_binary[0] == 1 && summed_binary[1] == 1)
+
+    summed_binary.join('')
+  end
+
 end
--- spec/lib/signed_decimal_to_binary_spec.rb
+++ spec/lib/signed_decimal_to_binary_spec.rb
@@ -3,9 +3,28 @@ require_relative '../../lib/signed_decimal_to_binary.rb'
 
 describe SignedDecimalToBinary do
 
-  it 'converts decimals to binaries' do
-    0.upto(10000) do |n|
-      expect(SignedDecimalToBinary.convert(n)).to eq("%b" % n)
+  describe '.convert' do
+    context 'when the decimal is positive' do
+      it 'converts a decimal to a binary' do
+        0.upto(1_00_000) do |n|
+          expect(SignedDecimalToBinary.convert(n)).to eq("%b" % n)
+        end
+      end
+    end
+    context 'when the decimal is negative' do
+      it 'converts a decimal to a binary' do
+        1.upto(1_00_000) do |n|
+          expect(SignedDecimalToBinary.convert(-n)).to eq ("%b" % (n*-1))[2..-1]
+        end
+      end
+    end
+  end
+
+  describe '.to_negative' do
+    it 'converts a binary string to its negative representation' do
+      1.upto(1_00_000) do |n|
+        expect(SignedDecimalToBinary.to_negative("%b" % n)).to eq ("%b" % (n*-1))[2..-1]
+      end
     end
   end
 
