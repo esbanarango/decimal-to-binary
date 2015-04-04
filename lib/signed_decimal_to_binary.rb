5b6ba720c9fc2256acfa5ed6319bd114ed596d0e
Esteba Arango <marranoparael31@gmail.com>
2015-04-03 23:15:31 -0500

    basic structure

---
+++ lib/signed_decimal_to_binary.rb
@@ -0,0 +1,3 @@
+class SignedDecimalToBinary
+
+end
---
+++ spec/lib/signed_decimal_to_binary_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require_relative '../../lib/signed_decimal_to_binary.rb'
+
+describe SignedDecimalToBinary do
+
+  let!(:decimal_to_binary) {SignedDecimalToBinary.new}
+
+end
