fa143ce3cb4bff3a045231dd182acf05e6454763
Esteban Arango Medina <marranoparael31@gmail.com>
2015-04-05 20:49:16 -0500

    Changing name and updating readme

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# Signed decimal to binary
+# Decimal to binary
 
 Implementation of the __Decimal to binary "problem"__. But enhanced.
 
+With this class you can convert ANY decimal number (positive or negative) to binary. And you can also use a negative base (-2).
 
+
+```` ruby
+decimal_to_binary = DecimalToBinary.new # Pass true as an agument if you want to use a negative base.
+decimal_to_binary.convert 200
+# "11001000"
+decimal_to_binary.convert -200
+# "100111000"
+
+decimal_to_binary.negative_base = true
+
+decimal_to_binary.convert 200
+# "000110111"
+decimal_to_binary.convert -200
+# "0001001011"
+
+```
 
 [![Build Status](https://travis-ci.org/esbanarango/signed-decimal-to-binary.svg)](https://travis-ci.org/esbanarango/signed-decimal-to-binary)
---
+++ spec/lib/decimal_to_binary_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require_relative '../../lib/decimal_to_binary.rb'
+
+describe DecimalToBinary do
+
+  let(:to_limit) { 1_00_000 }
+
+  context 'with positive base (2)' do
+
+    let(:decimal_to_binary) { DecimalToBinary.new }
+
+    describe '#convert' do
+      context 'when the decimal is positive' do
+        it 'converts a decimal to a binary' do
+          0.upto(to_limit) do |n|
+            expect(decimal_to_binary.convert(n)).to eq("%b" % n)
+          end
+        end
+      end
+      context 'when the decimal is negative' do
+        it 'converts a decimal to a binary' do
+          1.upto(to_limit) do |n|
+            expect(decimal_to_binary.convert(-n)).to eq ("%b" % (n*-1))[2..-1]
+          end
+        end
+      end
+    end
+
+    describe '#to_negative' do
+      it 'converts a binary string to its negative representation' do
+        1.upto(to_limit) do |n|
+          expect(decimal_to_binary.to_negative("%b" % n)).to eq ("%b" % (n*-1))[2..-1]
+        end
+      end
+    end
+
+  end
+
+  context 'with negative base (-2)' do
+
+    let(:decimal_to_binary) { DecimalToBinary.new true }
+
+    describe '#convert' do
+      it 'converts a decimal to binary with negative base' do
+        expect(decimal_to_binary.convert(-9)).to eq ('1101')
+        expect(decimal_to_binary.convert(9)).to eq ('10011')
+        expect(decimal_to_binary.convert(23)).to eq ('1101011')
+        expect(decimal_to_binary.convert(-23)).to eq ('100111')
+      end
+    end
+
+  end
+
+end
